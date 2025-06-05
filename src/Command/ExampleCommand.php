eed80e1d0aa3e1275d84c7f237fbb0b5cecba645
Aurélien Devaux <gitlab.aurelearn@aureldvx.net>
2025-06-05 09:38:59 +0200

    ref(upgrade:7.3): migrate commands

--- src/Command/ExampleCommand.php
+++ src/Command/ExampleCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -23,18 +24,18 @@ readonly class ExampleCommand
 
     public function __invoke(
         SymfonyStyle $io,
-        // #[Argument] string $email,
+        #[Argument] string $email,
     ): int {
         $user = new User();
         $user
-            // ->setEmail($email)
+            ->setEmail($email)
             ->setPassword($this->passwordHasher->hashPassword($user, 'password'))
         ;
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        // $io->success('User created successfully with email: ' . $email);
+        $io->success('User created successfully with email: ' . $email);
 
         return Command::SUCCESS;
     }
